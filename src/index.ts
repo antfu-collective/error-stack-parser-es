0251f27f1997b6755766d13e7f72b6a9d0f19f68
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-12 16:48:49 +0200

    feat: rewrite

--- src/index.ts
+++ src/index.ts
@@ -2,205 +2,265 @@
  * Port from https://github.com/stacktracejs/error-stack-parser-es
  */
 
-(function(root, factory) {
-    'use strict';
-    // Universal Module Definition (UMD) to support AMD, CommonJS/Node.js, Rhino, and browsers.
+// @ts-expect-error patched to remove the broken type
+import _StackFrameConstructor from 'stackframe'
 
-    /* istanbul ignore next */
-    if (typeof define === 'function' && define.amd) {
-        define('error-stack-parser-es', ['stackframe'], factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('stackframe'));
-    } else {
-        root.ErrorStackParser = factory(root.StackFrame);
+const StackFrameConstructor = _StackFrameConstructor as unknown as StackFrame
+
+export interface StackFrameOptions {
+  isConstructor?: boolean
+  isEval?: boolean
+  isNative?: boolean
+  isToplevel?: boolean
+  columnNumber?: number
+  lineNumber?: number
+  fileName?: string
+  functionName?: string
+  source?: string
+  args?: any[]
+  evalOrigin?: StackFrame
+}
+
+export interface StackFrame {
+  // eslint-disable-next-line @typescript-eslint/no-misused-new
+  new (obj: StackFrameOptions): StackFrame
+
+  args?: any[]
+  getArgs(): any[] | undefined
+  setArgs(args: any[]): void
+
+  evalOrigin?: StackFrame
+  getEvalOrigin(): StackFrame | undefined
+  setEvalOrigin(stackframe: StackFrame): void
+
+  isConstructor?: boolean
+  getIsConstructor(): boolean | undefined
+  setIsConstructor(isConstructor: boolean): void
+
+  isEval?: boolean
+  getIsEval(): boolean | undefined
+  setIsEval(isEval: boolean): void
+
+  isNative?: boolean
+  getIsNative(): boolean | undefined
+  setIsNative(isNative: boolean): void
+
+  isToplevel?: boolean
+  getIsToplevel(): boolean | undefined
+  setIsToplevel(isToplevel: boolean): void
+
+  columnNumber?: number
+  getColumnNumber(): number | undefined
+  setColumnNumber(columnNumber: number): void
+
+  lineNumber?: number
+  getLineNumber(): number | undefined
+  setLineNumber(lineNumber: number): void
+
+  fileName?: string
+  getFileName(): string | undefined
+  setFileName(fileName: string): void
+
+  functionName?: string
+  getFunctionName(): string | undefined
+  setFunctionName(functionName: string): void
+
+  source?: string
+  getSource(): string | undefined
+  setSource(source: string): void
+
+  toString(): string
+}
+
+const FIREFOX_SAFARI_STACK_REGEXP = /(^|@)\S+:\d+/
+const CHROME_IE_STACK_REGEXP = /^\s*at .*(\S+:\d+|\(native\))/m
+const SAFARI_NATIVE_CODE_REGEXP = /^(eval@)?(\[native code])?$/
+
+/**
+ * Given an Error object, extract the most information from it.
+ *
+ * @param {Error} error object
+ * @return {Array} of StackFrames
+ */
+export function parse(error: Error) {
+  // @ts-expect-error missing stacktrace property
+  if (typeof error.stacktrace !== 'undefined' || typeof error['opera#sourceloc'] !== 'undefined')
+    return parseOpera(error)
+
+  else if (error.stack && error.stack.match(CHROME_IE_STACK_REGEXP))
+    return parseV8OrIE(error)
+
+  else if (error.stack)
+    return parseFFOrSafari(error)
+
+  else
+    throw new Error('Cannot parse given Error object')
+}
+
+// Separate line and column numbers from a string of the form: (URI:Line:Column)
+export function extractLocation(urlLike: string) {
+  // Fail-fast but return locations like "(native)"
+  if (!urlLike.includes(':'))
+    return [urlLike]
+
+  const regExp = /(.+?)(?::(\d+))?(?::(\d+))?$/
+  const parts = regExp.exec(urlLike.replace(/[()]/g, ''))!
+  return [parts[1], parts[2] || undefined, parts[3] || undefined] as const
+}
+
+export function parseV8OrIE(error: Error) {
+  // @ts-expect-error missing stack property
+  const filtered = error.stack.split('\n').filter((line) => {
+    return !!line.match(CHROME_IE_STACK_REGEXP)
+  })
+
+  return filtered.map((line) => {
+    if (line.includes('(eval ')) {
+      // Throw away eval information until we implement stacktrace.js/stackframe#8
+      line = line.replace(/eval code/g, 'eval').replace(/(\(eval at [^()]*)|(,.*$)/g, '')
     }
-}(this, function ErrorStackParser(StackFrame) {
-    'use strict';
+    let sanitizedLine = line.replace(/^\s+/, '').replace(/\(eval code/g, '(').replace(/^.*?\s+/, '')
 
-    var FIREFOX_SAFARI_STACK_REGEXP = /(^|@)\S+:\d+/;
-    var CHROME_IE_STACK_REGEXP = /^\s*at .*(\S+:\d+|\(native\))/m;
-    var SAFARI_NATIVE_CODE_REGEXP = /^(eval@)?(\[native code])?$/;
+    // capture and preseve the parenthesized location "(/foo/my bar.js:12:87)" in
+    // case it has spaces in it, as the string is split on \s+ later on
+    const location = sanitizedLine.match(/ (\(.+\)$)/)
 
-    return {
-        /**
-         * Given an Error object, extract the most information from it.
-         *
-         * @param {Error} error object
-         * @return {Array} of StackFrames
-         */
-        parse: function ErrorStackParser$$parse(error) {
-            if (typeof error.stacktrace !== 'undefined' || typeof error['opera#sourceloc'] !== 'undefined') {
-                return this.parseOpera(error);
-            } else if (error.stack && error.stack.match(CHROME_IE_STACK_REGEXP)) {
-                return this.parseV8OrIE(error);
-            } else if (error.stack) {
-                return this.parseFFOrSafari(error);
-            } else {
-                throw new Error('Cannot parse given Error object');
-            }
-        },
+    // remove the parenthesized location from the line, if it was matched
+    sanitizedLine = location ? sanitizedLine.replace(location[0], '') : sanitizedLine
 
-        // Separate line and column numbers from a string of the form: (URI:Line:Column)
-        extractLocation: function ErrorStackParser$$extractLocation(urlLike) {
-            // Fail-fast but return locations like "(native)"
-            if (urlLike.indexOf(':') === -1) {
-                return [urlLike];
-            }
+    // if a location was matched, pass it to extractLocation() otherwise pass all sanitizedLine
+    // because this line doesn't have function name
+    const locationParts = extractLocation(location ? location[1] : sanitizedLine)
+    const functionName = (location && sanitizedLine) || undefined
+    const fileName = ['eval', '<anonymous>'].includes(locationParts[0]) ? undefined : locationParts[0]
 
-            var regExp = /(.+?)(?::(\d+))?(?::(\d+))?$/;
-            var parts = regExp.exec(urlLike.replace(/[()]/g, ''));
-            return [parts[1], parts[2] || undefined, parts[3] || undefined];
-        },
+    return new StackFrameConstructor({
+      functionName,
+      fileName,
+      lineNumber: +locationParts[1]!,
+      columnNumber: +locationParts[2]!,
+      source: line,
+    })
+  })
+}
 
-        parseV8OrIE: function ErrorStackParser$$parseV8OrIE(error) {
-            var filtered = error.stack.split('\n').filter(function(line) {
-                return !!line.match(CHROME_IE_STACK_REGEXP);
-            }, this);
+export function parseFFOrSafari(error: Error) {
+  // @ts-expect-error missing stack property
+  const filtered = error.stack.split('\n').filter((line) => {
+    return !line.match(SAFARI_NATIVE_CODE_REGEXP)
+  })
 
-            return filtered.map(function(line) {
-                if (line.indexOf('(eval ') > -1) {
-                    // Throw away eval information until we implement stacktrace.js/stackframe#8
-                    line = line.replace(/eval code/g, 'eval').replace(/(\(eval at [^()]*)|(,.*$)/g, '');
-                }
-                var sanitizedLine = line.replace(/^\s+/, '').replace(/\(eval code/g, '(').replace(/^.*?\s+/, '');
+  return filtered.map((line) => {
+    // Throw away eval information until we implement stacktrace.js/stackframe#8
+    if (line.includes(' > eval'))
+      line = line.replace(/ line (\d+)(?: > eval line \d+)* > eval:\d+:\d+/g, ':$1')
 
-                // capture and preseve the parenthesized location "(/foo/my bar.js:12:87)" in
-                // case it has spaces in it, as the string is split on \s+ later on
-                var location = sanitizedLine.match(/ (\(.+\)$)/);
+    if (!line.includes('@') && !line.includes(':')) {
+      // Safari eval frames only have function names and nothing else
+      return new StackFrameConstructor({
+        functionName: line,
+      })
+    }
+    else {
+      const functionNameRegex = /((.*".+"[^@]*)?[^@]*)(?:@)/
+      const matches = line.match(functionNameRegex)
+      const functionName = (matches && matches[1]) ? matches[1] : undefined
+      const locationParts = extractLocation(line.replace(functionNameRegex, ''))
 
-                // remove the parenthesized location from the line, if it was matched
-                sanitizedLine = location ? sanitizedLine.replace(location[0], '') : sanitizedLine;
+      return new StackFrameConstructor({
+        functionName,
+        fileName: locationParts[0],
+        lineNumber: +locationParts[1]!,
+        columnNumber: +locationParts[2]!,
+        source: line,
+      })
+    }
+  })
+}
 
-                // if a location was matched, pass it to extractLocation() otherwise pass all sanitizedLine
-                // because this line doesn't have function name
-                var locationParts = this.extractLocation(location ? location[1] : sanitizedLine);
-                var functionName = location && sanitizedLine || undefined;
-                var fileName = ['eval', '<anonymous>'].indexOf(locationParts[0]) > -1 ? undefined : locationParts[0];
+export function parseOpera(e: Error) {
+  // @ts-expect-error missing stacktrace property
+  if (!e.stacktrace || (e.message.includes('\n') && e.message.split('\n').length > e.stacktrace.split('\n').length))
+    return parseOpera9(e)
 
-                return new StackFrame({
-                    functionName: functionName,
-                    fileName: fileName,
-                    lineNumber: locationParts[1],
-                    columnNumber: locationParts[2],
-                    source: line
-                });
-            }, this);
-        },
+  else if (!e.stack)
+    return parseOpera10(e)
 
-        parseFFOrSafari: function ErrorStackParser$$parseFFOrSafari(error) {
-            var filtered = error.stack.split('\n').filter(function(line) {
-                return !line.match(SAFARI_NATIVE_CODE_REGEXP);
-            }, this);
+  else
+    return parseOpera11(e)
+}
 
-            return filtered.map(function(line) {
-                // Throw away eval information until we implement stacktrace.js/stackframe#8
-                if (line.indexOf(' > eval') > -1) {
-                    line = line.replace(/ line (\d+)(?: > eval line \d+)* > eval:\d+:\d+/g, ':$1');
-                }
+export function parseOpera9(e: Error) {
+  const lineRE = /Line (\d+).*script (?:in )?(\S+)/i
+  const lines = e.message.split('\n')
+  const result: StackFrame[] = []
 
-                if (line.indexOf('@') === -1 && line.indexOf(':') === -1) {
-                    // Safari eval frames only have function names and nothing else
-                    return new StackFrame({
-                        functionName: line
-                    });
-                } else {
-                    var functionNameRegex = /((.*".+"[^@]*)?[^@]*)(?:@)/;
-                    var matches = line.match(functionNameRegex);
-                    var functionName = matches && matches[1] ? matches[1] : undefined;
-                    var locationParts = this.extractLocation(line.replace(functionNameRegex, ''));
+  for (let i = 2, len = lines.length; i < len; i += 2) {
+    const match = lineRE.exec(lines[i])
+    if (match) {
+      result.push(new StackFrameConstructor({
+        fileName: match[2],
+        lineNumber: +match[1],
+        source: lines[i],
+      }))
+    }
+  }
 
-                    return new StackFrame({
-                        functionName: functionName,
-                        fileName: locationParts[0],
-                        lineNumber: locationParts[1],
-                        columnNumber: locationParts[2],
-                        source: line
-                    });
-                }
-            }, this);
-        },
+  return result
+}
 
-        parseOpera: function ErrorStackParser$$parseOpera(e) {
-            if (!e.stacktrace || (e.message.indexOf('\n') > -1 &&
-                e.message.split('\n').length > e.stacktrace.split('\n').length)) {
-                return this.parseOpera9(e);
-            } else if (!e.stack) {
-                return this.parseOpera10(e);
-            } else {
-                return this.parseOpera11(e);
-            }
-        },
+export function parseOpera10(e: Error) {
+  const lineRE = /Line (\d+).*script (?:in )?(\S+)(?:: In function (\S+))?$/i
+  // @ts-expect-error missing stack property
+  const lines = e.stacktrace.split('\n')
+  const result: StackFrame[] = []
 
-        parseOpera9: function ErrorStackParser$$parseOpera9(e) {
-            var lineRE = /Line (\d+).*script (?:in )?(\S+)/i;
-            var lines = e.message.split('\n');
-            var result = [];
+  for (let i = 0, len = lines.length; i < len; i += 2) {
+    const match = lineRE.exec(lines[i])
+    if (match) {
+      result.push(
+        new StackFrameConstructor({
+          functionName: match[3] || undefined,
+          fileName: match[2],
+          lineNumber: +match[1],
+          source: lines[i],
+        }),
+      )
+    }
+  }
 
-            for (var i = 2, len = lines.length; i < len; i += 2) {
-                var match = lineRE.exec(lines[i]);
-                if (match) {
-                    result.push(new StackFrame({
-                        fileName: match[2],
-                        lineNumber: match[1],
-                        source: lines[i]
-                    }));
-                }
-            }
+  return result
+}
 
-            return result;
-        },
+// Opera 10.65+ Error.stack very similar to FF/Safari
+export function parseOpera11(error: Error) {
+  // @ts-expect-error missing stack property
+  const filtered = error.stack.split('\n').filter((line) => {
+    return !!line.match(FIREFOX_SAFARI_STACK_REGEXP) && !line.match(/^Error created at/)
+  })
 
-        parseOpera10: function ErrorStackParser$$parseOpera10(e) {
-            var lineRE = /Line (\d+).*script (?:in )?(\S+)(?:: In function (\S+))?$/i;
-            var lines = e.stacktrace.split('\n');
-            var result = [];
+  return filtered.map((line) => {
+    const tokens = line.split('@')
+    const locationParts = extractLocation(tokens.pop()!)
+    const functionCall = (tokens.shift() || '')
+    const functionName = functionCall
+      .replace(/<anonymous function(: (\w+))?>/, '$2')
+      .replace(/\([^)]*\)/g, '') || undefined
+    let argsRaw
+    if (functionCall.match(/\(([^)]*)\)/))
+      argsRaw = functionCall.replace(/^[^(]+\(([^)]*)\)$/, '$1')
 
-            for (var i = 0, len = lines.length; i < len; i += 2) {
-                var match = lineRE.exec(lines[i]);
-                if (match) {
-                    result.push(
-                        new StackFrame({
-                            functionName: match[3] || undefined,
-                            fileName: match[2],
-                            lineNumber: match[1],
-                            source: lines[i]
-                        })
-                    );
-                }
-            }
+    const args = (argsRaw === undefined || argsRaw === '[arguments not available]')
+      ? undefined
+      : argsRaw.split(',')
 
-            return result;
-        },
-
-        // Opera 10.65+ Error.stack very similar to FF/Safari
-        parseOpera11: function ErrorStackParser$$parseOpera11(error) {
-            var filtered = error.stack.split('\n').filter(function(line) {
-                return !!line.match(FIREFOX_SAFARI_STACK_REGEXP) && !line.match(/^Error created at/);
-            }, this);
-
-            return filtered.map(function(line) {
-                var tokens = line.split('@');
-                var locationParts = this.extractLocation(tokens.pop());
-                var functionCall = (tokens.shift() || '');
-                var functionName = functionCall
-                    .replace(/<anonymous function(: (\w+))?>/, '$2')
-                    .replace(/\([^)]*\)/g, '') || undefined;
-                var argsRaw;
-                if (functionCall.match(/\(([^)]*)\)/)) {
-                    argsRaw = functionCall.replace(/^[^(]+\(([^)]*)\)$/, '$1');
-                }
-                var args = (argsRaw === undefined || argsRaw === '[arguments not available]') ?
-                    undefined : argsRaw.split(',');
-
-                return new StackFrame({
-                    functionName: functionName,
-                    args: args,
-                    fileName: locationParts[0],
-                    lineNumber: locationParts[1],
-                    columnNumber: locationParts[2],
-                    source: line
-                });
-            }, this);
-        }
-    };
-}));
+    return new StackFrameConstructor({
+      functionName,
+      args,
+      fileName: locationParts[0],
+      lineNumber: +locationParts[1]!,
+      columnNumber: +locationParts[2]!,
+      source: line,
+    })
+  })
+}
